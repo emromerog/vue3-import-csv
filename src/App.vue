363c5e7a6f6566552edba0de8df8c910760f1b2c
emromerog <emromerog@unal.edu.co>
2023-08-20 21:17:04 -0500

    chore: install tailwind dependency

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="flex flex-col items-center gap-5">
+    <div class="flex justify-center">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+</div>
+
 </template>
 
 <style scoped>
